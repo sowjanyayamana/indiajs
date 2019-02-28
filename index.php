012bec1e0e229eee1017236c636e81222761c4f3
sowjanyayamana <48083877+sowjanyayamana@users.noreply.github.com>
2019-02-28 14:43:45 +0530

    new

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+$method = $_SERVER['REQUEST_METHOD'];
+//Process only when method is POST
+if($method == "POST"){
+    $requestBody = file_get_contents('php://input');
+    $json = json_decode($requestBody);
+
+    $test = $json->result->parameters->text;
+
+    switch ($text{
+        case 'h1':
+            $speech = "Hi. Nice to meet you";
+            break;
+        case 'h2':
+            $speech ="Let us know your name please.";
+            break;
+        case 'h3':
+            $speech ="Where are you currently located?";
+            break;    
+        
+        default:
+            $speech ="Sorry, I didn't get that. Please ask me something else."
+         break;
+
+    })
+
+    $responce = new \stdClass();
+    $responce->speech = "";
+    $responce->displayText = "";
+    $responce->source ="webhook";
+    echo json_encode($responce);
+
+}
+else
+{
+    echo "Method not allowed";
+ 
+}
+
+>
